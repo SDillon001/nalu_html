2b92b44dd474a8466d37ddfaeebffeaa7b26e2cb
Steve Dillon <stevedillon001@gmail.com>
2018-10-11 22:24:22 -0700

    added down arrow

--- index.php
+++ index.php
@@ -7,85 +7,85 @@
 
 <body>
 
-<!--Included Header php file -->
-<?php include("header-include.php");?>
+  <!--Included Header php file -->
+  <?php include("header-include.php");?>
 
-<div class="wrapper">
+  <div class="wrapper">
     <div id="main" class="container">
 
       <!-- Section 1 -->
-    <div id="intro">
-      <div id="main-carousel" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="item active">
-            <a href="#"><img src="images/banners/sized/122_blake_1930.jpg" alt="122_blake_1930" /></a>
-            <div class="carousel-caption">
-              <div class="section content-wrapper2">
-                <h1>The History of Surfing</h1>
-                <h2>A Collective</h2>
-                <a class="btn caption section5_button" href="timeline.php">Explore the History</a>
+      <div id="intro">
+        <div id="main-carousel" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="item active">
+              <a href="#"><img src="images/banners/sized/page83_waikiki_1931.jpg" alt="122_blake_1930" /></a>
+              <div class="carousel-caption">
+                <div class="section content-wrapper2">
+                  <h1>The History of Surfing</h1>
+                  <h2>A Collective</h2>
+                  <a class="btn caption section5_button" href="timeline.php">Explore the History</a>
+                </div>
               </div>
-            </div> 
+            </div>
 
+            <!-- carousel image 1 -->
+            <div class="item">
+              <a href="#"><img src="images/banners/sized/122_blake_1930.jpg" alt="122_blake_1930.jpg" /></a>
+            </div>
+            <!-- carousel image 2 -->
+            <div class="item">
+              <a href="#"><img src="images/banners/sized/bolster_43_pipe_80s.jpg" alt="bolster_43_pipe_80s"/></a>
+            </div>
+            <!-- carousel image 3 -->
+            <div class="item">
+              <a href="#"><img src="images/banners/sized/bolster_stan.jpg" alt="bolster_stan.jpg" /></a>
+            </div>
+            <!-- carousel image 4 -->
+            <div class="item">
+              <a href="#"><img src="images/banners/sized/bolster_89.jpg" alt="bolster_89.jpg"/></a>
+            </div>
+            <!-- carousel image 5 -->
+            <div class="item">
+              <a href="#"><img src="images/banners/sized/bolster_wiameacrowd.jpg" alt="bolster_wiameacrowd.jpg" /></a>
+            </div>
+            <!-- carousel image 6 -->
+            <div class="item">
+              <a href="#"><img src="images/banners/sized/bolster_makahaparkinglotaerial.jpg" alt="bolster_makaha_parking_lot_aerial"/></a>
+            </div>
+            <!-- carousel image 7 -->
+            <div class="item">
+              <a href="#"><img src="images/banners/sized/bolster_71_tudor.jpg" alt="bolster_71_tudor" /></a>
+            </div>
+            <!-- carousel image 8 -->
+            <div class="item">
+              <a href="#"><img src="images/banners/sized/bolster_kewalobasindawn.jpg" alt="images/banners/sized/bolster_kewalobasindawn.jpg" /></a>
+            </div>
+            <!-- carousel image 9 -->
+            <div class="item">
+              <a href="#"><img src="images/banners/sized/blake5.jpg" alt="blake5.jpg" /></a>
+            </div>
+            <!-- carousel image 10 -->
+            <div class="item">
+              <a href="#"><img src="images/banners/sized/yellow_board_air.jpg" alt="yellow_board_air Salt Creek" /></a>
+            </div>
+            <!-- carousel image 11 -->
+            <div class="item">
+              <a href="#"><img src="images/banners/sized/page65_A_waikiki_1930.jpg" alt="page65_A_waikiki_1930" /></a>
+            </div>
+
+            <div class="arrow bounce">
+            <a href="#image-grid"><span class="glyphicon down"></span></a>
+          </div> 
           </div>
-          <!-- carousel image 1 -->
-          <div class="item">
-            <a href="#"><img src="images/banners/sized/082_duke_waikiki_1930_1.jpg" alt="duke_waikiki_1930"/></a>
-          </div>
-          <!-- carousel image 2 -->
-          <div class="item">
-            <a href="#"><img src="images/banners/sized/page83_waikiki_1931.jpg" alt="barrel2" /></a>
-          </div>
-          <!-- carousel image 3 -->
-          <div class="item">
-            <a href="#"><img src="images/banners/sized/bolster_43_pipe_80s.jpg" alt="bolster_43_pipe_80s"/></a>
-          </div>
-          <!-- carousel image 4 -->
-          <div class="item">
-            <a href="#"><img src="images/banners/sized/bolster_stan.jpg" alt="bolster_stan.jpg" /></a>
-          </div>
-          <!-- carousel image 5 -->
-          <div class="item">
-            <a href="#"><img src="images/banners/sized/bolster_89.jpg" alt="bolster_89.jpg"/></a>
-          </div>
-          <!-- carousel image 6 -->
-          <div class="item">
-            <a href="#"><img src="images/banners/sized/bolster_wiameacrowd.jpg" alt="bolster_wiameacrowd.jpg" /></a>
-          </div>
-          <!-- carousel image 7 -->
-          <div class="item">
-            <a href="#"><img src="images/banners/sized/bolster_makahaparkinglotaerial.jpg" alt="bolster_makaha_parking_lot_aerial"/></a>
-          </div>
-          <!-- carousel image 8 -->
-          <div class="item">
-            <a href="#"><img src="images/banners/sized/bolster_71_tudor.jpg" alt="bolster_71_tudor" /></a>
-          </div>
-          <!-- carousel image 9 -->
-          <div class="item">
-            <a href="#"><img src="images/banners/sized/bolster_kewalobasindawn.jpg" alt="images/banners/sized/bolster_kewalobasindawn.jpg" /></a>
-          </div>
-          <!-- carousel image 10 -->
-          <div class="item">
-            <a href="#"><img src="images/banners/sized/blake5.jpg" alt="blake5.jpg" /></a>
-          </div>
-          <!-- carousel image 11 -->
-          <div class="item">
-            <a href="#"><img src="images/banners/sized/yellow_board_air.jpg" alt="yellow_board_air Salt Creek" /></a>
-          </div>
-          <!-- carousel image 12 -->
-          <div class="item">
-            <a href="#"><img src="images/banners/sized/page65_A_waikiki_1930.jpg" alt="page65_A_waikiki_1930" /></a>
-          </div>
+
+          <a class="left carousel-control" data-target="#main-carousel" data-slide="prev">
+            <span class="glyphicon left"></span>
+          </a>
+          <a class="right carousel-control" data-target="#main-carousel" data-slide="next">
+            <span class="glyphicon right"></span>
+          </a>
         </div>
-
-        <a class="left carousel-control" data-target="#main-carousel" data-slide="prev">
-          <span class="glyphicon left"></span>
-        </a>
-        <a class="right carousel-control" data-target="#main-carousel" data-slide="next">
-          <span class="glyphicon right"></span>
-        </a>
       </div>
-    </div>
 
       <!-- Section 2 -->
       <div class="section" id="section2">
@@ -95,7 +95,7 @@
           <p class="section2_text"><span style="font-family: 'Raleway-SemiBold'">Nalu.live</span> is a website for surfers and non-surfers alike who love the sport of surfing and the culture that surrounds it. This website provides an interactive way for you to learn more about how the sport of surfing has evolved over the years and how innovations in surf photography have influenced some of those changes. Nalu is an educational site with a documentary feel – communicating content through beautiful photography, infographics, and videos to provide a fun, informative, and interactive experience.</p>
         </div>
 
-        <div class="flex-container">
+        <div class="flex-container" id="image-grid">
           <div class="imGrid">
             <div class="flex-item overlay">
               <a href="timeline.php#1900's"><img src="images/1900s_gurrey_surf_riders.jpg" alt="1900s Image Link">
@@ -146,27 +146,27 @@
       </div>
     </div>
 
-      <!-- Section 3  -->
-      <div class="section bcg-parallax" id="section3">
-        <div class="bcg"></div>
-        <div class="content-wrapper">
-          <h3>Warren Bolster</h3>
-          <h1>SURFING PIPELINE IN THE 70's</h1>
-          <h2>What it takes to surf and shoot the North Shore of Oahu</h2>
-          <a class="btn caption section5_button" href="timeline.php/#1970's">LEARN MORE</a>
-        </div>
+    <!-- Section 3  -->
+    <div class="section bcg-parallax" id="section3">
+      <div class="bcg"></div>
+      <div class="content-wrapper">
+        <h3>Warren Bolster</h3>
+        <h1>SURFING PIPELINE IN THE 70's</h1>
+        <h2>What it takes to surf and shoot the North Shore of Oahu</h2>
+        <a class="btn caption section5_button" href="timeline.php/#1970's">LEARN MORE</a>
       </div>
+    </div>
 
-      <!-- Section 4  -->
-      <div class="section" id="section4">
-        <div class="section4_content">
-          <img src="images/duke_olo_blake.png" alt="Duke Kahanamoku - Outrigger Canoe Club" >
-        </div>
-        <div class="col1">
-          <h2>Duke Kahanamoku</h2>
-          <p class="lead"><a href="https://eos.surf/entries/kahanamoku-duke" target="blank" >Surfing Icon</a></p>
+    <!-- Section 4  -->
+    <div class="section" id="section4">
+      <div class="section4_content">
+        <img src="images/duke_olo_blake.png" alt="Duke Kahanamoku - Outrigger Canoe Club" >
+      </div>
+      <div class="col1">
+        <h2>Duke Kahanamoku</h2>
+        <p class="lead"><a href="https://eos.surf/entries/kahanamoku-duke" target="blank" >Surfing Icon</a></p>
 
-          <article>
+        <article>
           <p>The long-celebrated father of modern surfing, from Honolulu, Hawaii; an Olympic gold medal swimmer and Hawaii's beloved ambassador to the world through much of the first half of the 20th century. Kahanamoku was a skilled wave-rider, but his real gift to surfing was the way he presented the sport as something that could be practiced with grace, humor, and generosity. "You know," he said in 1965, "there are so many waves coming in all the time, you don't have to worry about that. Take your time—wave come. Let the other guys go; catch another one." The sport's greatest shortcoming may be that surfers have for the most part failed to live up to the Kahanamoku ideal.</p>
 
           <p>Kahanamoku was born (1890) and raised in Honolulu, a full-blooded Hawaiian, and the first-born son of a delivery clerk. Five male siblings followed; all were first-rate surfers, swimmers, and paddlers. Surfing, along with other native Hawaiian forms of recreation, had all but vanished at the time of Kahanamoku's birth, partly because the indigenous population had been decimated by a century's worth of Western-borne disease, and partly due to the work-not-play influence of homesteading Calvinist missionaries. By the time Kahanamoku was in school, however, agricultural and tourist interests had replaced the missionaries in setting the political and cultural tone in Hawaii, and surfing began to flourish in Waikiki. It soon became the romantic symbol of America's newest vacation destination.</p>
@@ -175,59 +175,59 @@
 
           <p>In the 1912 Olympics, held in Stockholm, Sweden, the 6'1" 190-pound Duke used the already-famous "Kahanamoku Kick" to set another world record on his way to a gold medal in the 100-meter freestyle; he also won a silver medal in the 200-meter freestyle relay. In the 1920 Olympics in Antwerp, Belgium (World War I forced the cancellation of the 1916 Games), Kahanamoku won gold medals in both the 100-meter freestyle and the 400-meter freestyle relay; in the 1924 Olympics in Paris, the 34-year-old won a silver medal in the 100-meter freestyle. He was called the "human fish" and the "Bronzed Duke," and at age 42 Kahanamoku swam sprints as fast as when he was 21. In 1925, he made what the Honolulu Star-Bulletin described as a "superhuman rescue act," pulling eight fishermen out of heavy seas at Newport Beach, California.</p>
           <div class="read-more-link">
-          <a href="/equipment.php" class="home-lm-link">Read More</a>
+            <a href="/equipment.php" class="home-lm-link">Read More</a>
           </div>
-          </article>
+        </article>
 
-        </div>
+      </div>
+    </div>
+
+    <!-- Section 5  -->
+    <div class="section fp-auto-height" id="section5">
+      <div class="section5_content">
+        <h2>Beneath the Chaos</h2>
+        <h1>UNDERWATER SURF PHOTOGRAPHY</h1>
+        <h1>Art | Passion</h1>
+        <a class="btn caption section5_button" id="sec5-but" href="timeline.php">LEARN MORE</a>
+      </div>
+    </div>
+
+    <!-- Section 6  -->
+    <div class="section fp-auto-height" id="section6">
+      <h3 id="section0"></h3>
+      <div class="section6_content">
+        <h2>Tradition | Culture | Stoke</h2>
+        <h1>EXPLORE THE SPORT OF SURFING</h1>
+        <hr>
       </div>
 
-      <!-- Section 5  -->
-      <div class="section fp-auto-height" id="section5">
-        <div class="section5_content">
-          <h2>Beneath the Chaos</h2>
-          <h1>UNDERWATER SURF PHOTOGRAPHY</h1>
-          <h1>Art | Passion</h1>
-          <a class="btn caption section5_button" id="sec5-but" href="timeline.php">LEARN MORE</a>
-        </div>
-      </div>
+      <div class="flex-container2">
+        <div class="imGrid2">
+          <div class="flex-item2"><a href="/timeline.php"><img src="images/1958_huntington_pier.jpg" alt="1958_huntington_pier"></a>
+            <h3>Timeline</h3>
+            <p>Sed sapien ex, egestas vel elit non, fringilla maximus odio. Sed in est vulputate, fermentum massa vel, lacinia justo.</p>
+            <a href="/timeline.php" class="home-lm-link">Learn More</a>
+          </div>
 
-      <!-- Section 6  -->
-      <div class="section fp-auto-height" id="section6">
-        <h3 id="section0"></h3>
-        <div class="section6_content">
-          <h2>Tradition | Culture | Stoke</h2>
-          <h1>EXPLORE THE SPORT OF SURFING</h1>
-          <hr>
-        </div>
+          <div class="flex-item2"><a href="/photographers.php"><img src="images/bolster_tavarua_empty_perfect.jpg" alt="bolster_tavarua_empty_perfect"></a>
+            <h3>Photo</h3>
+            <p>Sed sapien ex, egestas vel elit non, fringilla maximus odio. Sed in est vulputate, fermentum massa vel, lacinia justo.</p>
+            <a href="/photo.php" class="home-lm-link">Learn More</a>
+          </div>
 
-        <div class="flex-container2">
-          <div class="imGrid2">
-            <div class="flex-item2"><a href="/timeline.php"><img src="images/1958_huntington_pier.jpg" alt="1958_huntington_pier"></a>
-              <h3>Timeline</h3>
-              <p>Sed sapien ex, egestas vel elit non, fringilla maximus odio. Sed in est vulputate, fermentum massa vel, lacinia justo.</p>
-              <a href="/timeline.php" class="home-lm-link">Learn More</a>
-            </div>
-
-            <div class="flex-item2"><a href="/photographers.php"><img src="images/bolster_tavarua_empty_perfect.jpg" alt="bolster_tavarua_empty_perfect"></a>
-              <h3>Photo</h3>
-              <p>Sed sapien ex, egestas vel elit non, fringilla maximus odio. Sed in est vulputate, fermentum massa vel, lacinia justo.</p>
-              <a href="/photo.php" class="home-lm-link">Learn More</a>
-            </div>
-
-            <div class="flex-item2"><a href="/equipment.php"><img src="images/142_tom_blake_hollow_surfboard_grid.jpg" alt="tom_blake_hollow_surfboard_plans"></a>
-              <h3>Equipment</h3>
-              <p>Sed sapien ex, egestas vel elit non, fringilla maximus odio. Sed in est vulputate, fermentum massa vel, lacinia justo.</p>
-              <a href="/equipment.php" class="home-lm-link">Learn More</a>
-            </div>
+          <div class="flex-item2"><a href="/equipment.php"><img src="images/142_tom_blake_hollow_surfboard_grid.jpg" alt="tom_blake_hollow_surfboard_plans"></a>
+            <h3>Equipment</h3>
+            <p>Sed sapien ex, egestas vel elit non, fringilla maximus odio. Sed in est vulputate, fermentum massa vel, lacinia justo.</p>
+            <a href="/equipment.php" class="home-lm-link">Learn More</a>
           </div>
         </div>
       </div>
+    </div>
 
-      <!--Included Header php file -->
-      <?php include("footer-include.php");?>
+    <!--Included Header php file -->
+    <?php include("footer-include.php");?>
 
-    </body>
-  </div>
+  </body>
+</div>
 
-    </html>
+</html>
